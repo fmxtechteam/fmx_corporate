a2ddc0dd97ef459c415d1320a0b2523666ecfb0d
olatubora-felix <olatubora1996@gmail.com>
2025-03-10 12:15:04 +0100

    Added Output for static site generation

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
   staticPageGenerationTimeout: 1000,
   images: {
     domains: ["res.cloudinary.com"],
+    unoptimized: true,
   },
+  output: "export",
 };
 
 export default nextConfig;
